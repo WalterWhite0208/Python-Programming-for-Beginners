ec94142f4e01cc61c73f3a86f63a6c95bcf42d73
ByteSlinger0307 <dubeykrish0307@gmail.com>
2024-09-22 17:15:04 +0530

    Source code of Video - 4

---
+++ 04_Understanding Variables & Datatypes/datatypes.py
@@ -0,0 +1,20 @@
+#integer
+age = 12
+print("Age is:", age)
+print(type(age))
+
+# Float
+height = 5.7
+print("Height is:", height)
+print(type(height))
+
+# String 
+name = "Krish"
+print("Name is:", name)
+print(type(name))
+
+# Boolean
+is_stdent = True
+print("Is a student:", is_stdent)
+print(type(is_stdent))
+
---
+++ 04_Understanding Variables & Datatypes/variables.py
@@ -0,0 +1,7 @@
+x = 5
+y = "Hello World"
+z = 3.14
+
+print(x)
+print(y)
+print(z)
